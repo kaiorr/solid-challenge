d3c216d73e350dd1009eeb417c4f0df32b6e5c5f
kaiorr <kaio.rocharibeiro@gmail.com>
2022-02-08 22:45:40 -0300

    feat: add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -6,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", usersRoutes);
 
 export { app };
